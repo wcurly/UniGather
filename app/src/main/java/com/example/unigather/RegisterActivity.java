2eb8fd9933d010985dee373c941d62e07f547364
wcurly <1540852074@qq.com>
2023-12-13 23:54:50 +0800

    注册页面改进

--- app/src/main/java/com/example/unigather/RegisterActivity.java
+++ app/src/main/java/com/example/unigather/RegisterActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Button;
 import android.widget.EditText;
 
 // RegisterActivity.java
 public class RegisterActivity extends AppCompatActivity {
 
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private int timeLeft = 30; // 倒计时秒数
+    Button buttonGetCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,12 @@ public class RegisterActivity extends AppCompatActivity {
         EditText editTextNewPassword = findViewById(R.id.editTextNewPassword);
         EditText editTextNewEmail = findViewById(R.id.editTextNewEmail);
         Button buttonCreateAccount = findViewById(R.id.buttonCreateAccount);
+        buttonGetCode = findViewById(R.id.buttonGetCode);
+
+        buttonGetCode.setOnClickListener(v->{
+            //
+            startCountdown();
+        });
 
         buttonCreateAccount.setOnClickListener(v -> {
             // 注册逻辑
@@ -26,5 +37,24 @@ public class RegisterActivity extends AppCompatActivity {
             startActivity(intent);
             finish(); // 关闭注册页面
         });
+    }
+
+    private void startCountdown() {
+        buttonGetCode.setEnabled(false); // 禁用按钮
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (timeLeft > 0) {
+                    buttonGetCode.setText(timeLeft + "s");
+                    timeLeft--;
+                    handler.postDelayed(this, 1000); // 每秒更新一次
+                } else {
+                    buttonGetCode.setText("获取验证码");
+                    buttonGetCode.setEnabled(true); // 倒计时结束，重新启用按钮
+                    timeLeft = 30; // 重置倒计时
+                }
+            }
+        };
+        handler.post(runnable);
     }
 }
